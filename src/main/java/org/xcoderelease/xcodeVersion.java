7a1f3178203f80066598c5b3d370a18389c9c677
yogesh khandelwal <er.yogeshkhandelwal@gmail.com>
2020-09-17 00:45:08 +0530

    Fixing path and resulting only latest three values

--- src/main/java/org/xcoderelease/xcodeVersion.java
+++ src/main/java/org/xcoderelease/xcodeVersion.java
@@ -32,7 +32,7 @@ public class xcodeVersion {
                 if(line == null){
                     break;
                 }else{
-                    if(line[28].equals("true")) {
+                    if(line[29].equals("true")) {
                         resultstokeep=resultstokeep+1;
                         sb.append("xcodeversion = " + line[24] +"\n");
                         sb.append("Release date = " + line[2] + "-" + line[0] + "-" + line[1] +"\n");
@@ -67,6 +67,7 @@ public class xcodeVersion {
                 reader = new CSVReader(new FileReader(csvFile));
                 String[] line;
                 int count = 1;
+                int resultstokeep =1;
                 String version = null;
                 StringBuffer sb = new StringBuffer();
                 //As first line is data header so set to skip
@@ -87,6 +88,7 @@ public class xcodeVersion {
                         compareXcodeVersion.VersionStr vs2 = new compareXcodeVersion.VersionStr(line[24]);
                         int result = vs2.compareTo(vs1);
                         if (result == 0) {
+                            resultstokeep=resultstokeep+1;
                             sb.append("xcodeversion = " + line[24] + "\n");
                             sb.append("Release date = " + line[2] + "-" + line[0] + "-" + line[1] + "\n");
                             if (!line[28].equals("")) {
@@ -95,6 +97,9 @@ public class xcodeVersion {
                             sb.append("DownloadURL = " + line[4] + "\n");
                             sb.append("RequireOS = " + line[27] + "\n");
                             sb.append("Build Number = " + line[25] + "\n");
+                        }
+                        if(resultstokeep==4){
+                            break;
                         }
                     }
                     count++;
